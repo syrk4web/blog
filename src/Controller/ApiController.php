25c2c247eeecd87b77c64bba7c3ddc7e0f1909a6
Jordan Blasenhauer <blasenhauer.jordan.dev@gmail.com>
2024-04-10 11:16:04 +0200

    add api endpoint

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -38,4 +38,35 @@ class ApiController extends AbstractController
             ]);
         }
     }
+
+    
+    #[Route(path:"/api/article/{id}", name:"api_article", methods: ['GET'])]
+    public function api_article(int $id, EntityManagerInterface $entityManager): Response {
+        try {
+            // Get list of articles
+            $article = $entityManager->getRepository(Article::class)->find($id);
+            $data = [];
+            // Determine data to send on API
+            if($article) {
+                $data = [
+                        'title' => $article->getTitle(),
+                        'content' => $article->getContent(),
+                        'date' => $article->getDate(),
+                        'id' => $article->getId(),
+                    ];
+            } else {
+                $data = [
+                    'message' => 'Article not found'
+                ];
+            }
+            
+            // Format to json
+            $json = json_encode($data);
+            // Send response (data, status, header)
+            return new Response($json, 200, ['Content-Type'=> 'application/json']);
+        }catch(e) {
+            // Case error
+            return new Response('{}', 500, ['Content-Type'=> 'application/json']);
+        }
+    }
 }
